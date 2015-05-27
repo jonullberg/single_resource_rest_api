49ac4d42e21e6d6717393b587b9b431812f677da
Jonathan Ullberg <jonathan.ullberg@gmail.com>
2015-05-27 10:29:37 -0700

    added a client side to the notes app

---
+++ app/js/client.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var intro = require('./introduction');
+var notesGet = require('./notes_get');
+
+document.write(intro());
+document.write(notesGet());
---
+++ router/notes_routes.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var Note = require('../model/Note');	// pulls in the Quote schema
+var bodyparser = require('body-parser'); // requires in the bodyparser module
+
+module.exports = function(router) {
+	var fiveHundred = function(err, res) {
+		console.log(err);
+		return res.status(500).json({msg: 'internal server error'});
+	};
+
+
+	router.use(bodyparser.json());
+
+	router.get('/notes', function(req, res) {
+		Note.find({}, function(err, data) {
+			if(err) {
+				fiveHundred(err, res);
+			}
+
+			res.json(data);	
+		});
+	});
+
+	router.post('/notes', function(req, res) {
+		var newNote = new Note(req.body);
+		newNote.save(function(err, data) {
+
+			if(err){
+				var inputTitle = Object.keys(err.errors)[0];
+				var errPath = err.errors[inputTitle];
+				var errMsg = {msg: 'invalid input for ' + errPath.path};
+				if(errPath.kind === 'required') {
+					var fourHundred = function(err, res) {
+						console.log(err.errors[inputTitle].message);
+						return res.status(400).json(errMsg);
+					};
+					return fourHundred(err, res);
+					
+				}
+				fiveHundred(err, res);
+			}
+			res.json(data);
+		});
+	});
+
+	router.put('/notes/:id', function(req, res) {
+		var updatedNote = req.body;
+		delete updatedNote._id;
+
+		Note.update({_id: req.params.id}, updatedNote, function(err) {
+			if(err) {
+				fiveHundred(err, res);
+			}
+			res.json({msg: 'success'});
+		});
+	});
+
+	router.delete('/notes/:id', function(req, res) {
+		Note.remove({'_id': req.params.id}, function(err, data) {
+			if (err) {
+				console.log(err);
+				return res.status(500).json({msg: 'internal server error'});
+			}
+
+			res.json({msg: 'success'});
+		});
+	});
+};
